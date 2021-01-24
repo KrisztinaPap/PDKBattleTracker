a2fb555de97dc3e8329370008dfa207deb932b64
Krisztina Pap <krisztina.pap@outlook.com>
2021-01-23 17:08:04 -0700

    Initial commit with gitignore file

---
+++ PDKBattleTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PDKBattleTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
